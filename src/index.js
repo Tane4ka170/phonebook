d056de8723563f404fc2465540247052ea408b1e
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-02-20 18:42:59 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-08-phonebook">
+    <BrowserRouter basename="/phonebook">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
